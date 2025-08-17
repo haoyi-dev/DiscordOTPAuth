42a12303aea08adb1f517d4ae48500379c73403a
HaoyiDev <haoyi.dev@gmail.com>
2025-08-17 23:54:08 +0700

    haoyi-dev

--- src/main/java/haoyidev/discordotpauth/listeners/PlayerJoinListener.java
+++ src/main/java/haoyidev/discordotpauth/listeners/PlayerJoinListener.java
@@ -15,6 +15,14 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+        if (!plugin.getAuthManager().isPendingAuth(event.getPlayer().getUniqueId())) {
+            plugin.getAuthManager().addRestrictedPlayer(event.getPlayer().getUniqueId());
+            event.getPlayer().sendMessage("§6[OTP] Bạn chưa xác thực!");
+            event.getPlayer().sendMessage("§6[OTP] $f/otp send <discord_id> -> Gửi OTP về Discord");
+            event.getPlayer().sendMessage("§6[OTP] $f/otp <mã_otp> -> Nhập mã OTP trong game");
+            event.getPlayer().sendMessage("§6[OTP] $7Bạn sẽ bị giới hạn hành động cho đến khi xác thực!");
+        }
         IPManager ipManager = plugin.getIpManager();
         ipManager.recordPlayerIP(event.getPlayer());
     }
